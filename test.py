a14e6b0994ed6da36a42d9ddd3882a60ee20d7b2
Viktor Kopp <VKopp@luxoft.com>
2018-08-02 18:23:10 +0200

    Prepared 'general.html' template

--- test.py
+++ test.py
@@ -1,7 +1,34 @@
 from jinja2 import Environment, FileSystemLoader
 import os
 from collections import namedtuple
+import datetime
+import dateutils
 
+project_data = {
+    "name": "Repostat",
+    "url": "https://github.com/vifactor/repostat",
+    "version": "X.Y.Z",
+    "tools": ["python v.X", "git v.Y", "gnuplot v.Z"],
+    "age": 456,
+    "active_days_count": 11,
+    "active_days_ratio": "{0:.2f}".format(float(11) / 456 * 100),
+    "commits_count": 100,
+    "commits_per_active_day_count": "{0:.2f}".format(float(100) / 11),
+    "commits_per_day_count": "{0:.2f}".format(float(100) / 456),
+    "authors_count": 30,
+    "commits_per_author_count": "{0:.2f}".format(float(100) / 30),
+    "files_count": 128,
+    "total_lines_count": 1204,
+    "added_lines_count": 5032,
+    "removed_lines_count": 5032 - 1024,
+    "first_commit_date": datetime.datetime.today() - dateutils.relativedelta(months=13),
+    "last_commit_date": datetime.datetime.today() - dateutils.relativedelta(months=2, days=5)
+    }
+
+generation_data = {
+    "datetime": datetime.datetime.today(),
+    "duration": 10
+}
 
 if __name__ == '__main__':
     # Capture our current directory
@@ -14,9 +41,8 @@ if __name__ == '__main__':
     output_dir = os.path.join(code_dir, 'output')
     for template in j2_env.list_templates(filter_func=lambda template_name: template_name != 'base.html'):
         template_rendered = j2_env.get_template(template).render(
-            version="abc",
-            page_title=os.path.basename(template),
-            text = template
+            project=project_data,
+            generation=generation_data
         )
         print template
 
