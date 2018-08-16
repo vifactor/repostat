c4af49f5b3f0c18cbd6c8d620deca1e08c8c829d
Viktor Kopp <vifactor@gmail.com>
2018-08-16 16:11:58 +0200

    Separated tags info estimation and moved to test.py Tags info retrieval is implemented using pygit2 Test prepared to control the equivalence of two methods pygit2-method is used in gitstats

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import datetime
+import re
 
 from tools import GitStatistics
 from tools import get_pipe_output
@@ -31,6 +33,48 @@ def getcommitrange(defaultrange = 'HEAD', end_only = False):
         return '%s..%s' % (conf['commit_begin'], conf['commit_end'])
     return defaultrange
 
+
+def get_tags_info():
+    tags = {}
+    lines = get_pipe_output(['git show-ref --tags']).split('\n')
+    for line in lines:
+        if len(line) == 0:
+            continue
+        (hash, tag) = line.split(' ')
+
+        tag = tag.replace('refs/tags/', '')
+        output = get_pipe_output(['git log "%s" --pretty=format:"%%at %%aN" -n 1' % hash])
+        if len(output) > 0:
+            parts = output.split(' ')
+            try:
+                stamp = int(parts[0])
+            except ValueError:
+                stamp = 0
+            tags[tag] = {'stamp': long(stamp), 'hash': hash,
+                              'date': datetime.datetime.fromtimestamp(stamp).strftime('%Y-%m-%d'), 'commits': 0,
+                              'authors': {}}
+
+    # collect info on tags, starting from latest
+    tags_sorted_by_date_desc = map(lambda el: el[1],
+                                   reversed(sorted(map(lambda el: (el[1]['date'], el[0]), tags.items()))))
+    prev = None
+    for tag in reversed(tags_sorted_by_date_desc):
+        cmd = 'git shortlog -s "%s"' % tag
+        if prev is not None:
+            cmd += ' "^%s"' % prev
+        output = get_pipe_output([cmd])
+        if len(output) == 0:
+            continue
+        prev = tag
+        for line in output.split('\n'):
+            parts = re.split('\s+', line, 2)
+            commits = int(parts[1])
+            author = parts[2]
+            tags[tag]['commits'] += commits
+            tags[tag]['authors'][author] = commits
+    return tags
+
+
 class TestPygitMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -40,6 +84,21 @@ class TestPygitMethods(unittest.TestCase):
     def test_authors_count(self):
         gitstats_retrieval_result = int(get_pipe_output(['git shortlog -s %s' % getlogrange(), 'wc -l']))
         self.assertEqual(gitstats_retrieval_result, len(self.gs.authors))
+
+    def test_tags_info(self):
+        expected_tags_dict = get_tags_info()
+        actual_tags_dict = self.gs.tags
+
+        self.assertListEqual(expected_tags_dict.keys(), actual_tags_dict.keys(),
+                             "Tags list is not same estimated by different methods")
+        for tagname in expected_tags_dict.keys():
+            expected_authors = expected_tags_dict[tagname]['authors']
+            actual_authors = actual_tags_dict[tagname]['authors']
+            # direct comparison of the two dicts is problematic
+            # due to difference in unicode strings parsing via command line and pygit2
+            self.assertEquals(len(expected_authors), len(actual_authors))
+            self.assertEquals(expected_tags_dict[tagname]['commits'], actual_tags_dict[tagname]['commits'])
+
 
 if __name__ == '__main__':
     unittest.main()
--- tools/gitstatistics.py
+++ tools/gitstatistics.py
@@ -1,5 +1,5 @@
 import pygit2 as git
-
+import datetime
 
 class GitStatistics:
     def __init__(self, path):
@@ -7,8 +7,32 @@ class GitStatistics:
         :param path: path to a repository
         """
         self.repo = git.Repository(path)
-        self.authors = {}
-        self.fetch_authors()
+        self.authors = self.fetch_authors()
+        self.tags = self.fetch_tags_info()
 
     def fetch_authors(self):
-        self.authors = {commit.author.name for commit in self.repo.walk(self.repo.head.target)}
+        return {commit.author.name for commit in self.repo.walk(self.repo.head.target)}
+
+    def fetch_tags_info(self):
+        tags = filter(lambda refobj: refobj.name.startswith('refs/tags'), self.repo.listall_reference_objects())
+        commit_tag = {refobj.peel().oid: refobj.shorthand for refobj in tags}
+
+        result = {refobj.shorthand: {
+            'stamp': refobj.peel().author.time,
+            'date': datetime.datetime.fromtimestamp(refobj.peel().author.time).strftime('%Y-%m-%d'),
+            'hash': str(refobj.target)} for refobj in tags}
+
+        authors = {}
+        commit_count = 0
+        for commit in self.repo.walk(self.repo.head.target, git.GIT_SORT_TOPOLOGICAL | git.GIT_SORT_REVERSE):
+            commit_count += 1
+            authors[commit.author.name] = authors.get(commit.author.name, 0) + 1
+            if commit.oid in commit_tag.keys():
+                tagname = commit_tag[commit.oid]
+                result[tagname]['commits'] = commit_count
+                result[tagname]['authors'] = authors
+
+                commit_count = 0
+                authors = {}
+
+        return result
