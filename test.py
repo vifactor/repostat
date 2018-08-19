571c774b64f64b772f5d7a088ae0b112d8cee944
Viktor Kopp <vifactor@gmail.com>
2018-08-19 19:12:02 +0200

    Authors info retrieval is implemented using pygit2 Test prepared to control the equivalence of two methods
    Used pygit2 method to calculated authors info

--- test.py
+++ test.py
@@ -20,13 +20,15 @@ conf = {
     'start_date': ''
 }
 
-def getlogrange(defaultrange = 'HEAD', end_only = True):
+
+def getlogrange(defaultrange='HEAD', end_only=True):
     commit_range = getcommitrange(defaultrange, end_only)
     if len(conf['start_date']) > 0:
         return '--since="%s" "%s"' % (conf['start_date'], commit_range)
     return commit_range
 
-def getcommitrange(defaultrange = 'HEAD', end_only = False):
+
+def getcommitrange(defaultrange='HEAD', end_only=False):
     if len(conf['commit_end']) > 0:
         if end_only or len(conf['commit_begin']) == 0:
             return conf['commit_end']
@@ -51,8 +53,8 @@ def get_tags_info():
             except ValueError:
                 stamp = 0
             tags[tag] = {'stamp': long(stamp), 'hash': hash,
-                              'date': datetime.datetime.fromtimestamp(stamp).strftime('%Y-%m-%d'), 'commits': 0,
-                              'authors': {}}
+                         'date': datetime.datetime.fromtimestamp(stamp).strftime('%Y-%m-%d'), 'commits': 0,
+                         'authors': {}}
 
     # collect info on tags, starting from latest
     tags_sorted_by_date_desc = map(lambda el: el[1],
@@ -75,15 +77,103 @@ def get_tags_info():
     return tags
 
 
+def get_stat_summary_counts(line):
+    numbers = re.findall('\d+', line)
+    if len(numbers) == 1:
+        # neither insertions nor deletions: may probably only happen for "0 files changed"
+        numbers.append(0)
+        numbers.append(0)
+    elif len(numbers) == 2 and line.find('(+)') != -1:
+        numbers.append(0)  # only insertions were printed on line
+    elif len(numbers) == 2 and line.find('(-)') != -1:
+        numbers.insert(1, 0)  # only deletions were printed on line
+    return numbers
+
+
+def get_authors_info():
+    authors = {}
+
+    # Collect revision statistics
+    # Outputs "<stamp> <date> <time> <timezone> <author> '<' <mail> '>'"
+    lines = get_pipe_output(
+        ['git rev-list --pretty=format:"%%at %%ai %%aN <%%aE>" %s' % getlogrange('HEAD'), 'grep -v ^commit']).split(
+        '\n')
+    for line in lines:
+        parts = line.split(' ', 4)
+        try:
+            stamp = int(parts[0])
+        except ValueError:
+            stamp = 0
+        author, mail = parts[4].split('<', 1)
+        author = author.rstrip()
+        date = datetime.datetime.fromtimestamp(float(stamp))
+        # author stats
+        if author not in authors:
+            authors[author] = {}
+        # commits, note again that commits may be in any date order because of cherry-picking and patches
+        if 'last_commit_stamp' not in authors[author]:
+            authors[author]['last_commit_stamp'] = stamp
+        if stamp > authors[author]['last_commit_stamp']:
+            authors[author]['last_commit_stamp'] = stamp
+        if 'first_commit_stamp' not in authors[author]:
+            authors[author]['first_commit_stamp'] = stamp
+        if stamp < authors[author]['first_commit_stamp']:
+            authors[author]['first_commit_stamp'] = stamp
+
+        # authors: active days
+        yymmdd = date.strftime('%Y-%m-%d')
+        if 'last_active_day' not in authors[author]:
+            authors[author]['last_active_day'] = yymmdd
+            authors[author]['active_days'] = set([yymmdd])
+        elif yymmdd != authors[author]['last_active_day']:
+            authors[author]['last_active_day'] = yymmdd
+            authors[author]['active_days'].add(yymmdd)
+
+    lines = get_pipe_output(
+        ['git log --shortstat --date-order --pretty=format:"%%at %%aN" %s' % (getlogrange('HEAD'))]).split('\n')
+    lines.reverse()
+
+    inserted = 0
+    deleted = 0
+    stamp = 0
+    for line in lines:
+        if len(line) == 0:
+            continue
+        # <stamp> <author>
+        if re.search('files? changed', line) is None:
+            pos = line.find(' ')
+            if pos != -1:
+                try:
+                    oldstamp = stamp
+                    (stamp, author) = (int(line[:pos]), line[pos + 1:])
+                    if oldstamp > stamp:
+                        # clock skew, keep old timestamp to avoid having ugly graph
+                        stamp = oldstamp
+                    if author not in authors:
+                        authors[author] = {'lines_added': 0, 'lines_removed': 0, 'commits': 0}
+                    authors[author]['commits'] = authors[author].get('commits', 0) + 1
+                    authors[author]['lines_added'] = authors[author].get('lines_added', 0) + inserted
+                    authors[author]['lines_removed'] = authors[author].get('lines_removed', 0) + deleted
+                    files, inserted, deleted = 0, 0, 0
+                except ValueError:
+                    print 'Warning: unexpected line "%s"' % line
+            else:
+                print 'Warning: unexpected line "%s"' % line
+        else:
+            numbers = get_stat_summary_counts(line)
+            if len(numbers) == 3:
+                (files, inserted, deleted) = map(lambda el: int(el), numbers)
+            else:
+                print 'Warning: failed to handle line "%s"' % line
+                (files, inserted, deleted) = (0, 0, 0)
+    return authors
+
+
 class TestPygitMethods(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         this_file_dir = os.path.dirname(os.path.abspath(__file__))
         cls.gs = GitStatistics(this_file_dir)
-
-    def test_authors_count(self):
-        gitstats_retrieval_result = int(get_pipe_output(['git shortlog -s %s' % getlogrange(), 'wc -l']))
-        self.assertEqual(gitstats_retrieval_result, len(self.gs.authors))
 
     def test_tags_info(self):
         expected_tags_dict = get_tags_info()
@@ -98,6 +188,34 @@ class TestPygitMethods(unittest.TestCase):
             # due to difference in unicode strings parsing via command line and pygit2
             self.assertEquals(len(expected_authors), len(actual_authors))
             self.assertEquals(expected_tags_dict[tagname]['commits'], actual_tags_dict[tagname]['commits'])
+
+    def test_authors_info(self):
+        expected_authors_dict = get_authors_info()
+        actual_authors_dict = self.gs.authors
+        # due to string encoding differences direct comparison of author's statistics is not possible
+        self.assertEquals(len(expected_authors_dict), len(actual_authors_dict))
+
+        # the lists of added and removed lines by authors is compared instead
+        # if something is wrong in stat algo, coincidence of the numbers is highly unlikely
+        expected_lines_added_list = [v['lines_added'] for (_, v) in expected_authors_dict.items()]
+        actual_lines_added_list = [v['lines_added'] for (_, v) in actual_authors_dict.items()]
+        self.assertListEqual(sorted(actual_lines_added_list), sorted(expected_lines_added_list))
+
+        expected_lines_removed_list = [v['lines_removed'] for (_, v) in expected_authors_dict.items()]
+        actual_lines_removed_list = [v['lines_removed'] for (_, v) in actual_authors_dict.items()]
+        self.assertListEqual(sorted(actual_lines_removed_list), sorted(expected_lines_removed_list))
+
+        expected_commits_count_list = [v['commits'] for (_, v) in expected_authors_dict.items()]
+        actual_commits_count_list = [v['commits'] for (_, v) in actual_authors_dict.items()]
+        self.assertListEqual(sorted(expected_commits_count_list), sorted(actual_commits_count_list))
+
+        expected_active_days_count_list = [len(v['active_days']) for (_, v) in expected_authors_dict.items()]
+        actual_active_days_count_list = [len(v['active_days']) for (_, v) in actual_authors_dict.items()]
+        self.assertListEqual(sorted(expected_active_days_count_list), sorted(actual_active_days_count_list))
+
+        expected_last_commit_stamp = [v['last_commit_stamp'] for (_, v) in expected_authors_dict.items()]
+        actual_last_commit_stamp = [v['last_commit_stamp'] for (_, v) in actual_authors_dict.items()]
+        self.assertListEqual(sorted(expected_last_commit_stamp), sorted(actual_last_commit_stamp))
 
 
 if __name__ == '__main__':
--- tools/gitstatistics.py
+++ tools/gitstatistics.py
@@ -1,6 +1,7 @@
 import pygit2 as git
 import datetime
 
+
 class GitStatistics:
     def __init__(self, path):
         """
@@ -11,7 +12,51 @@ class GitStatistics:
         self.tags = self.fetch_tags_info()
 
     def fetch_authors(self):
-        return {commit.author.name for commit in self.repo.walk(self.repo.head.target)}
+        """
+        e.g.
+        {'Stefano Mosconi': {'lines_removed': 1, 'last_commit_stamp': 1302027851, 'active_days': set(['2011-04-05']),
+                             'lines_added': 1, 'commits': 1, 'first_commit_stamp': 1302027851,
+                             'last_active_day': '2011-04-05'}
+        """
+        result = {}
+        for child_commit in self.repo.walk(self.repo.head.target, git.GIT_SORT_TIME):
+            is_merge_commit = False
+            if len(child_commit.parents) == 0:
+                # initial commit
+                st = child_commit.tree.diff_to_tree(swap=True).stats
+            elif len(child_commit.parents) == 1:
+                parent_commit = child_commit.parents[0]
+                st = self.repo.diff(parent_commit, child_commit).stats
+            else:  # if len(child_commit.parents) == 2 (merge commit)
+                is_merge_commit = True
+            commit_day_str = datetime.datetime.fromtimestamp(child_commit.author.time).strftime('%Y-%m-%d')
+            author_name = child_commit.author.name.encode('utf-8')
+            if author_name not in result:
+                result[author_name] = {
+                    'lines_removed': st.deletions if not is_merge_commit else 0,
+                    'lines_added': st.insertions if not is_merge_commit else 0,
+                    'active_days': {commit_day_str},
+                    'commits': 1,
+                    'first_commit_stamp': child_commit.author.time,
+                    'last_commit_stamp': child_commit.author.time,
+                }
+            else:
+                result[author_name]['lines_removed'] += st.deletions if not is_merge_commit else 0
+                result[author_name]['lines_added'] += st.insertions if not is_merge_commit else 0
+                result[author_name]['active_days'].add(commit_day_str)
+                result[author_name]['commits'] += 1
+                if result[author_name]['first_commit_stamp'] > child_commit.author.time:
+                    result[author_name]['first_commit_stamp'] = child_commit.author.time
+                if result[author_name]['last_commit_stamp'] < child_commit.author.time:
+                    result[author_name]['last_commit_stamp'] = child_commit.author.time
+
+        # it seems that there is a mistake (or my misunderstanding) in 'last_active_day' value
+        # my calculations give are not the same as those done by Heikki Hokkanen for this parameter
+        for author in result:
+            last_active_day = datetime.datetime.fromtimestamp(result[author]['last_commit_stamp']).strftime('%Y-%m-%d')
+            result[author]['last_active_day'] = last_active_day
+
+        return result
 
     def fetch_tags_info(self):
         tags = filter(lambda refobj: refobj.name.startswith('refs/tags'), self.repo.listall_reference_objects())
