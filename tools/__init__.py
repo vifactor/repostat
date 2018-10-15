d3bc2c62679c3ec2df57970e0c3ca263bbaa0219
Jan Beran <jberan@redhat.com>
2018-10-15 12:53:11 +0200

    py3 compatibility: Adjust import
    In Python 3, imports can be either absolute or explicitly relative;
    implicit relative imports are forbidden.
    
    Signed-off-by: Jan Beran <jberan@redhat.com>

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,3 @@
 from .shellhelper import get_pipe_output, get_external_execution_time
 from .gitstatistics import GitStatistics
-from versioning import generate_version_from, fetch_contributors_from
+from .versioning import generate_version_from, fetch_contributors_from
