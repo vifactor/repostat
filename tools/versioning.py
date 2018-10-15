41075919daf487fa99145f3805c3de4a41e05537
Viktor Kopp <VKopp@luxoft.com>
2018-10-15 10:26:16 +0200

    Fix for failure when submodule is added to the project.
    https://github.com/vifactor/repostat/issues/29
    
    repo.blame raises an error if path of a submodule is given as an
    argument

--- tools/versioning.py
+++ tools/versioning.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 def generate_version_from(commit, hex_oid_symbols_count=7):
     commit_yymm = datetime.fromtimestamp(commit.author.time).strftime('%Y-%m')
     commit_hash = commit.hex[:hex_oid_symbols_count]
@@ -9,12 +8,16 @@ def generate_version_from(commit, hex_oid_symbols_count=7):
 
 def fetch_contributors_from(repo, commit):
     contribution = {}
+    submodules_paths = repo.listall_submodules()
     for p in commit.tree.diff_to_tree():
-        blob_blame = repo.blame(p.delta.new_file.path)
-        for blame_hunk in blob_blame:
-            contribution[blame_hunk.final_committer.name] = contribution.get(blame_hunk.final_committer.name, 0) \
-                                                            + blame_hunk.lines_in_hunk
+        file_to_blame = p.delta.new_file.path
+        if file_to_blame not in submodules_paths:
+            blob_blame = repo.blame(file_to_blame)
+            for blame_hunk in blob_blame:
+                contribution[blame_hunk.final_committer.name] = contribution.get(blame_hunk.final_committer.name, 0) \
+                                                                + blame_hunk.lines_in_hunk
 
     # this gives only the contributors to the current code tree, not all the commiters to the project
     contribution = sorted(contribution.items(), key=lambda kv: kv[1], reverse=True)
+
     return [contributor for contributor, lines_contributed in contribution]
