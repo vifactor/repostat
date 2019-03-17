abfdbe071dca3a03247365d8363de9eb5108cfb3
Gábor Bereczki <bere.gabor@gmail.com>
2019-03-17 21:10:52 +0100

    csvreportcreator unit test correction after refact

--- test/test_csvreportcreator.py
+++ test/test_csvreportcreator.py
@@ -1,10 +1,8 @@
 import unittest
 import shutil
 import os
-from tools.csvreportcreator   import AuthorCsvExporter
-from tools.csvreportcreator   import CommitCsvExporter
-from tools.csvreportcreator import CSVReportCreator
-from tools.csvreportcreator import GeneralDictionaryExport
+from tools.csvreportcreator import DictionaryListCsvExporter
+from tools.csvreportcreator import DictionaryCsvExporter
 from tools.gitstatistics import GitStatistics
 
 class BasicTestExporter(unittest.TestCase):
@@ -21,30 +19,31 @@ class BasicTestExporter(unittest.TestCase):
         print('Init git repo. Path:' + this_file_dir)
         self.gs = GitStatistics(this_file_dir)
 
-class TestAuthorExporter(BasicTestExporter):
+class TestDictionaryListCsvExporte(BasicTestExporter):
 
-    def testAuthorExport(self):
-        exporter = AuthorCsvExporter()
+    def testAuthorDictExport(self):
+        #test with dict
+        exporter = DictionaryListCsvExporter()
         fileName = os.path.join(self.csv_outputdir, 'authors.csv')
         additional = {'projectname': 'Project Name Unit test', 'reponame': 'repostat'}
-        exporter.exportAuthors(fileName, self.gs.authors, additional)
+        exporter.export(fileName, self.gs.authors, additional)
         self.assertTrue(os.path.isfile(fileName))
         self.assertTrue('Viktor Kopp;4028;3238' in open(fileName).read())
 
-class TestCommitExporter(BasicTestExporter):
-
-    def testCommitExport(self):
-        exporter = CommitCsvExporter()
+    def testCommitListExport(self):
+        #test with list
+        exporter = DictionaryListCsvExporter()
         fileName = os.path.join(self.csv_outputdir, 'commits.csv')
         additional = {'projectname': 'Project Name Unit test', 'reponame': 'repostat'}
-        exporter.exportCommits(fileName, self.gs.commits, additional)
+        exporter.export(fileName, self.gs.commits, additional)
         self.assertTrue(os.path.isfile(fileName))
-        self.assertTrue('Viktor Kopp;5;8;2018-10-15' in open(fileName).read())
+        self.assertTrue('Viktor Kopp;8;5;2018-10-15' in open(fileName).read())
 
-class TestGeneralDictionaryExport(BasicTestExporter):
 
-    def testGeneralDictionaryExportWithoutKey(self):
-        exporter = GeneralDictionaryExport()
+class TestGeneralDictionaryCsvExporter(BasicTestExporter):
+
+    def testGeneralDictionaryCsvExporterWithoutKey(self):
+        exporter = DictionaryCsvExporter()
         fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withoutKey.csv')
         testData = {}
         for i in range (0, 10):
@@ -53,8 +52,8 @@ class TestGeneralDictionaryExport(BasicTestExporter):
         self.assertTrue(os.path.isfile(fileName))
         self.assertTrue('projectname;reponame' in open(fileName).read())
 
-    def testGeneralDictionaryExportWithKeyDefault(self):
-        exporter = GeneralDictionaryExport()
+    def testGeneralDictionaryCsvExporterWithKeyDefault(self):
+        exporter = DictionaryCsvExporter()
         fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withkeydef.csv')
         testData = {}
         for i in range (0, 10):
@@ -64,8 +63,8 @@ class TestGeneralDictionaryExport(BasicTestExporter):
         self.assertTrue('projectname;reponame;key' in open(fileName).read())
         self.assertTrue('Project Name Unit test;repostat;0' in open(fileName).read())
 
-    def testGeneralDictionaryExportWithKeyUnit(self):
-        exporter = GeneralDictionaryExport()
+    def testGeneralDictionaryCsvExporterWithKeyUnit(self):
+        exporter = DictionaryCsvExporter()
         fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withkeyunit.csv')
         testData = {}
         for i in range (0, 10):
