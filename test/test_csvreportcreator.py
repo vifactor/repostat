5535054b6c4fde7460e2d6d18bdebe4d1863292f
Gábor Bereczki <bere.gabor@gmail.com>
2019-03-17 15:29:12 +0100

    add GeneralDictionaryExport unit test

--- test/test_csvreportcreator.py
+++ test/test_csvreportcreator.py
@@ -4,6 +4,7 @@ import os
 from tools.csvreportcreator   import AuthorCsvExporter
 from tools.csvreportcreator   import CommitCsvExporter
 from tools.csvreportcreator import CSVReportCreator
+from tools.csvreportcreator import GeneralDictionaryExport
 from tools.gitstatistics import GitStatistics
 
 class BasicTestExporter(unittest.TestCase):
@@ -40,3 +41,36 @@ class TestCommitExporter(BasicTestExporter):
         self.assertTrue(os.path.isfile(fileName))
         self.assertTrue('Viktor Kopp;5;8;2018-10-15' in open(fileName).read())
 
+class TestGeneralDictionaryExport(BasicTestExporter):
+
+    def testGeneralDictionaryExportWithoutKey(self):
+        exporter = GeneralDictionaryExport()
+        fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withoutKey.csv')
+        testData = {}
+        for i in range (0, 10):
+            testData[i] = {'projectname': 'Project Name Unit test', 'reponame': 'repostat'}
+        exporter.export(fileName, testData, False)
+        self.assertTrue(os.path.isfile(fileName))
+        self.assertTrue('projectname;reponame' in open(fileName).read())
+
+    def testGeneralDictionaryExportWithKeyDefault(self):
+        exporter = GeneralDictionaryExport()
+        fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withkeydef.csv')
+        testData = {}
+        for i in range (0, 10):
+            testData[i] = {'projectname': 'Project Name Unit test', 'reponame': 'repostat'}
+        exporter.export(fileName, testData)
+        self.assertTrue(os.path.isfile(fileName))
+        self.assertTrue('projectname;reponame;key' in open(fileName).read())
+        self.assertTrue('Project Name Unit test;repostat;0' in open(fileName).read())
+
+    def testGeneralDictionaryExportWithKeyUnit(self):
+        exporter = GeneralDictionaryExport()
+        fileName = os.path.join(self.csv_outputdir, 'general_dict_export_withkeyunit.csv')
+        testData = {}
+        for i in range (0, 10):
+            testData[i] = {'projectname': 'Project Name Unit test', 'reponame': 'repostat'}
+        exporter.export(fileName, testData, True, 'Key UnitTestField')
+        self.assertTrue(os.path.isfile(fileName))
+        self.assertTrue('projectname;reponame;Key UnitTestField' in open(fileName).read())
+        self.assertTrue('Project Name Unit test;repostat;0' in open(fileName).read())
