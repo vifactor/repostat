67c0bd2cc869c8ee742a2e977715bc7793626a3f
Viktor Kopp <VKopp@luxoft.com>
2019-03-21 14:17:19 +0100

    Add installation script

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+# name with underscore is used here because 'repostat' is already occupied by https://pypi.org/project/repostat/
+setup(name='repo_stat',
+      version='0.91',
+      description='Desktop git repository analyser and report creator.',
+      keywords='git analisys statistics',
+      url='https://github.com/vifactor/repostat',
+      author='Viktor Kopp',
+      author_email='vifactor(at)gmail.com',
+      license='GPLv3',
+      packages=['tools'],
+      install_requires=[
+            'cffi==1.11.5',
+            'dateutils==0.6.6',
+            'Jinja2==2.10',
+            'MarkupSafe==1.0',
+            'pygit2>=0.24.2,<0.28',
+            'python-dateutil==2.7.3',
+            'pytz==2018.5',
+            'six==1.11.0'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['gitstats'],
+      include_package_data=True,
+      zip_safe=False)
