c4c57d9ca29be8b87dd90b0b99a90f32553a73fa
Viktor Kopp <VKopp@luxoft.com>
2019-03-29 18:55:07 +0100

    Add check of a recent git tag for 'releaseness'
    Tag of type 'v[number].[number].[number]' is a release tag
    last number corresponding to patch version is optional

--- git_hooks/push_release.py
+++ git_hooks/push_release.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 import sys
+import subprocess
+import re
 
-print('pre-push hook has been called!')
+# TODO: do not perform any actions if not on 'master' branch
+# get most recent tag name in current branch
+git_tag_string = subprocess.check_output(['git', 'describe', '--abbrev=0', '--tags']).decode().strip()
+
+# check if this is a version tag of type: vN.N[.N], e.g. "v0.1"
+
+if re.fullmatch(r'^v\d+\.\d+(\.\d+)?\Z', git_tag_string):
+    print('the most recent version tag is:', git_tag_string)
+
+# NOTE: for debug purposes (without push) use exit code different from 0
 sys.exit(0)
