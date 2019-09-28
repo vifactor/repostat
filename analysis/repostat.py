75db147bbec608ae244d9f574e6e2858fa4a9ab7
Viktor Kopp <vifactor@gmail.com>
2019-09-28 23:48:47 +0200

    Simplify repostat launch-script
    Remove redundant GitStat class and some calls to chdir

--- analysis/repostat.py
+++ analysis/repostat.py
@@ -18,74 +18,58 @@ os.environ['LC_ALL'] = 'C'
 time_start = time.time()
 
 
-class GitStats:
-
-    def run(self, args_orig):
-        try:
-            config = Configuration(args_orig)
-            args = config.get_args()
-        except ConfigurationException as ce:
-            warnings.warn("Configuration exception occurred:")
-            warnings.warn(ce)
-            sys.exit(1)
-
-        # check gnuplot version needed to HTML reports
-        if config.is_html_output() and not config.is_valid_gnuplot_version():
-            warnings.warn("Invalid gnuplot version. Required "
-                          "minimal version: %s. Current version: %s" % (Configuration.GNUPLOT_MINIMAL_VERSION,
-                                                                        config.get_gnuplot_version()))
-            sys.exit(1)
-
-        output_path = args.output_path
-        run_dir = config.get_run_dir()
-
-        print('Output path: %s' % output_path)
-
-        print('Collecting data...')
-        data = GitDataCollector(config.get_args_dict(), args.git_repo)
-
-        # todo: Check loop result. It seems every loop rewrite the collected information in data object.
-        #  Is this loop really needed?
-        # for git_repo in args.git_repo:
-        print('Git path: %s' % args.git_repo)
-
-        prevdir = os.getcwd()
-        os.chdir(args.git_repo)
-        os.chdir(prevdir)
-
-        print('Refining data...')
-        data.refine()
-
-        os.chdir(run_dir)
-        print('Generating report...')
-        # fixme: pass GitStatistics object directly when obsolete GitDataCollector is removed
-        if config.is_html_output():
-            print('Generating HTML report...')
-            report = HTMLReportCreator(config, data.repo_statistics)
-            report.create(data, output_path)
-            if sys.stdin.isatty():
-                print('You may now run:')
-                print('')
-                print('   sensible-browser \'%s\'' % os.path.join(output_path, 'general.html').replace("'", "'\\''"))
-                print('')
-        elif config.is_csv_output():
-            print('Generating CSV report...')
-            report = CSVReportCreator()
-            report.create(data.repo_statistics, output_path, config.get_args_dict(), config.is_append_csv())
-            print('CSV report created here: %s' % output_path)
-        self.get_times()
-
-    @staticmethod
-    def get_times():
-        time_end = time.time()
-        exectime_internal = time_end - time_start
-        exectime_external = get_external_execution_time()
-        print('Execution time %.5f secs, %.5f secs (%.2f %%) in external commands)'
-              % (exectime_internal, exectime_external, (100.0 * exectime_external) / exectime_internal))
+def print_exec_times():
+    time_end = time.time()
+    exectime_internal = time_end - time_start
+    exectime_external = get_external_execution_time()
+    print('Execution time %.5f secs, %.5f secs (%.2f %%) in external commands)'
+          % (exectime_internal, exectime_external, (100.0 * exectime_external) / exectime_internal))
 
 
 def main():
-    GitStats().run(sys.argv[1:])
+    try:
+        config = Configuration(sys.argv[1:])
+        args = config.get_args()
+    except ConfigurationException as ce:
+        warnings.warn("Configuration exception occurred:")
+        warnings.warn(ce)
+        sys.exit(1)
+
+    # check gnuplot version needed to HTML reports
+    if config.is_html_output() and not config.is_valid_gnuplot_version():
+        warnings.warn("Invalid gnuplot version. Required "
+                      "minimal version: %s. Current version: %s" % (Configuration.GNUPLOT_MINIMAL_VERSION,
+                                                                    config.get_gnuplot_version()))
+        sys.exit(1)
+
+    print('Git path: %s' % args.git_repo)
+    print('Collecting data...')
+    data = GitDataCollector(config.get_args_dict(), args.git_repo)
+
+    print('Refining data...')
+    data.refine()
+
+    output_path = args.output_path
+    print('Output path: %s' % output_path)
+
+    os.chdir(config.get_run_dir())
+
+    # fixme: pass GitStatistics object directly when obsolete GitDataCollector is removed
+    if config.is_html_output():
+        print('Generating HTML report...')
+        report = HTMLReportCreator(config, data.repo_statistics)
+        report.create(data, output_path)
+        if sys.stdin.isatty():
+            print('You may now run:')
+            print('')
+            print('   sensible-browser \'%s\'' % os.path.join(output_path, 'general.html').replace("'", "'\\''"))
+            print('')
+    elif config.is_csv_output():
+        print('Generating CSV report...')
+        report = CSVReportCreator()
+        report.create(data.repo_statistics, output_path, config.get_args_dict(), config.is_append_csv())
+        print('CSV report created here: %s' % output_path)
+    print_exec_times()
 
 
 if __name__ == '__main__':
