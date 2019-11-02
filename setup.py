55e078a8b918b3f556e0430f74931984134b36e7
Viktor Kopp <vifactor@gmail.com>
2019-11-02 23:48:56 +0100

    Initial snap packaging #64
    Build passed, assets missing (copies to "share" folder)

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+from glob import glob
+
 from tools.configuration import Configuration
 
 # name with underscore is used here because 'repostat' is already occupied by https://pypi.org/project/repostat/
@@ -18,17 +20,21 @@ setup(name='repo_stat',
           "Topic :: Software Development :: Version Control",
           "Topic :: Utilities"
       ],
+      data_files=[("share/repostat/gnuplot", glob("gnuplot/*.plot")),
+                  ("share/repostat/templates", glob("templates/*.html")),
+                  ("share/repostat/assets/images", glob("assets/**/*.gif", recursive=True)),
+                  ("share/repostat/assets", ["assets/gitrepostat.css", "assets/sortable.js"]),
+                  ("share/repostat", ["release_data.json"])],
       packages=['analysis', 'tools'],
       install_requires=[
           'cffi==1.11.5',
           'Jinja2>=2.10.1',
           'MarkupSafe==1.0',
-          'pygit2>=0.24.2,<=0.28',
+          'pygit2==0.26.2',
           'pytz==2018.5',
           'six>=1.11.0'
       ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
       entry_points={"console_scripts": ["repostat = analysis.repostat:main"]},
       include_package_data=True,
-      zip_safe=False)
+      zip_safe=False
+)
