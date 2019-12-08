c0067a3add438de20b550c81697d1901643426f8
Viktor Kopp <vifactor@gmail.com>
2019-12-08 21:58:53 +0100

    Add test checking no raise in case of empty author's email
    Issue #84

--- test/test_single_commit_stats.py
+++ test/test_single_commit_stats.py
@@ -38,6 +38,28 @@ class TestSingleCommitRepository(unittest.TestCase):
         self.assertEqual(author_stats['date_last'], today)
         # total activity days count is not checked
 
+    def test_missing_authors_email(self):
+        import tempfile
+        import os
+        from tools import shellhelper
+
+        cwd = os.getcwd()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # change working directory
+            os.chdir(tmpdir)
+            shellhelper.get_pipe_output(['git init'])
+            shellhelper.get_pipe_output(['touch file.txt'])
+            shellhelper.get_pipe_output(['git add file.txt'])
+            # apparently it is not possible to create pygit.Signature with empty author's email (and name)
+            # but commits with no author's email can be created via git
+            shellhelper.get_pipe_output(['git commit -m "No-email author" --author "Author NoEmail <>"'])
+            try:
+                # Commit without author's email does not crash statistics calculation
+                GitStatistics(tmpdir)
+            except Exception as e:
+                self.fail(str(e))
+            os.chdir(cwd)
+
 
 if __name__ == '__main__':
     unittest.main()
