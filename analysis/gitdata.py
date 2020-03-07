10b7c2153e65d09fb2aca174711e083dfd0e1f50
Viktor Kopp <vifactor@gmail.com>
2020-03-07 10:53:38 +0100

    Introduce new GitRepository statistics
    Calculate first/last timestamps

--- analysis/gitdata.py
+++ analysis/gitdata.py
@@ -18,7 +18,7 @@ class History:
         repo_walker = self._get_repo_walker()
         records = []
         for commit in repo_walker:
-            records.append({'commit_sha': commit.hex, 'author_timestamp': commit.author.time})
+            records.append({'commit_sha': commit.hex[:7], 'author_timestamp': commit.author.time})
         return records
 
     @abc.abstractmethod
--- analysis/tests/test_repo_statistics.py
+++ analysis/tests/test_repo_statistics.py
@@ -1,15 +1,36 @@
 import unittest
 from unittest.mock import patch, MagicMock
+
 from analysis.gitdata import WholeHistory
+from analysis.gitrepository import GitRepository
 
 
 class RepoStatisticsTest(unittest.TestCase):
     test_whole_history_records = [
-        [1],
+        {'commit_sha': '6c40597', 'author_timestamp': 1580666336},
+        {'commit_sha': '358604e', 'author_timestamp': 1583449674},
+        {'commit_sha': 'fdc28ab', 'author_timestamp': 1185807283}
     ]
 
     @patch.object(WholeHistory, 'fetch', return_value=test_whole_history_records)
-    def test_raw_data_fetched(self, mock_fetch):
+    def test_whole_history_fetched(self, mock_fetch):
         df = WholeHistory(MagicMock()).as_dataframe()
         mock_fetch.assert_called()
-        self.assertEqual(len(self.test_whole_history_records), df.size)
+        self.assertEqual(len(self.test_whole_history_records), len(df.index))
+
+    @patch.object(WholeHistory, 'fetch', return_value=test_whole_history_records)
+    def test_whole_history_once_for_statistics(self, mock_fetch):
+        with patch("pygit2.Repository"):
+            GitRepository(MagicMock())
+            self.assertEqual(1, mock_fetch.call_count)
+
+    @patch.object(WholeHistory, 'fetch', return_value=test_whole_history_records)
+    def test_first_last_timestamps(self, mock_fetch):
+        with patch("pygit2.Repository"):
+            timestamps = [rec['author_timestamp'] for rec in self.test_whole_history_records]
+            expected_last_commit_timestamp = max(timestamps)
+            expected_first_commit_timestamp = min(timestamps)
+
+            stat = GitRepository(MagicMock())
+            self.assertEqual(expected_first_commit_timestamp, stat.first_commit_timestamp)
+            self.assertEqual(expected_last_commit_timestamp, stat.last_commit_timestamp)
