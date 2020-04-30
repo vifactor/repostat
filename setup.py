8e75a82b1001ac4485e5dc4496680c3067f7fafc
Viktor Kopp <vifactor@gmail.com>
2020-04-30 23:40:34 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='repostat-app',
           "Environment :: Console",
           "Intended Audience :: Developers",
           "Intended Audience :: Science/Research",
-          "Intended Audience :: Education"
+          "Intended Audience :: Education",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
