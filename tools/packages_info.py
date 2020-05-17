61047a071f8a8015445b1813f7a2cb5470b73943
Viktor Kopp <vifactor@gmail.com>
2020-05-17 14:18:25 +0200

    Release v2.1.1

--- tools/packages_info.py
+++ tools/packages_info.py
@@ -3,8 +3,8 @@ import jinja2
 
 
 def get_pygit2_info():
-    return f'{pygit2.__name__} v.{pygit2.__version__} (backed by libgit2 v.{pygit2.LIBGIT2_VERSION})'
+    return f'{pygit2.__name__} v{pygit2.__version__} (backed by libgit2 v{pygit2.LIBGIT2_VERSION})'
 
 
 def get_jinja_info():
-    return f'{jinja2.__name__} v.{jinja2.__version__}'
+    return f'{jinja2.__name__} v{jinja2.__version__}'
