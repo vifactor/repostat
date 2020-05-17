e4d6a2b3c464abc587b179fad48d9a223943d907
Viktor Kopp <vifactor@gmail.com>
2020-05-17 11:49:19 +0200

    Fix information about pygit2 version in "About"-page
    Move package version retrieving methods to own module

---
+++ tools/packages_info.py
@@ -0,0 +1,10 @@
+import pygit2
+import jinja2
+
+
+def get_pygit2_info():
+    return f'{pygit2.__name__} v.{pygit2.__version__} (backed by libgit2 v.{pygit2.LIBGIT2_VERSION})'
+
+
+def get_jinja_info():
+    return f'{jinja2.__name__} v.{jinja2.__version__}'
